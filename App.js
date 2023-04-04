8263c6fbd4e8b9539dc25a60cd926132b922a582
GOKULMATHI <108513958+GOKULMATHI@users.noreply.github.com>
2023-04-04 23:03:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Home from './Home'
+import NavBar from './Navbar';
+import Singers from './Singers';
+import Albums from './Albums';
+
+import { Route,Routes } from 'react-router-dom';
+
+
+export default function App() {
+  return (
+    <> 
+      <div>
+      <NavBar/>
+        <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route path='/singers' element={<Singers />}></Route>
+            <Route path='/albums' element={<Albums />}></Route>
+        </Routes>
+      </div>
+    </>
+  )
+}
